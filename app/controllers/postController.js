f7fb62848ad1109c36603aa77cf7461b7ad27dc1
Sumod Sivadas <contactsumod@gmail.com>
2020-07-08 21:44:54 +0530

    Added html index page

--- app/controllers/postController.js
+++ app/controllers/postController.js
@@ -1,13 +1,26 @@
-var app = angular.module("insert",[]);
-app.controller("controller", function($scope, $http) {
-    $scope.insert = function(){
-      $http.post(
-          "api/insertData.php",
-          {'name':$scope.name, 'age':$scope.age, 'email':$scope.email}
-          ).then(function(data){
-            $scope.name = null;
-            $scope.age =null;
-            $scope.email=null;
-        });
+/*angular.module('todomvc')
+	.controller('TodoCtrl', function TodoCtrl($scope, $routeParams, $filter, store) {
+		'use strict';
+
+		var todos = $scope.todos = store.todos;
+
+
+$scope.addTodo = function () {
+    var newTodo = {
+        title: $scope.newTodo.trim(),
+        completed: false
+    };
+
+    if (!newTodo.title) {
+        return;
     }
-});
+
+    $scope.saving = true;
+    store.insert(newTodo)
+        .then(function success() {
+            $scope.newTodo = '';
+        })
+        .finally(function () {
+            $scope.saving = false;
+        });
+}; */
